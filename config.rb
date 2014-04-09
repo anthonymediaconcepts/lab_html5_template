f5ce7d146404829ce2f29cc556aa99ddbf3ff9ea
Anthony Balao <anthony@mediaconcepts.com>
2014-04-09 09:57:03 +0800

    First draft files

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Config.rb
+# Set this to the root of your project when deployed.
+http_path = "/"
+css_dir = "./css"
+sass_dir = "sass"
+images_dir = "./img"
+javascripts_dir = "././js"
+
+# output_style = :expanded or :nested or :compact or :compressed
+# relative_assets = true
+
+#output_style = :compressed
+line_comments = false;
+
+# preferred_syntax = :sass
